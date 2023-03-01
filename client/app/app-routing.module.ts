4848f9545c2f6b8837df85da634f173297e62074
GoldFrite <goldfrite@gmail.com>
2023-03-01 16:44:13 +0100

    Client • Left panel

--- client/app/app-routing.module.ts
+++ client/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app/app.component';
 import { ConfigurationRoutingModule } from './configuration/configuration-routing.module';
 import { HomeRoutingModule } from './home/home-routing.module';
 import { NotFoundComponent } from './not-found/not-found.component';
@@ -7,6 +8,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 const routes: Routes = [
   ...HomeRoutingModule.routes,
   ...ConfigurationRoutingModule.routes,
+  { path: '', component: AppComponent },
   { path: '**', component: NotFoundComponent, pathMatch: 'full' },
 ];
 
--- client/app/home/left-panel/left-panel.component.ts
+++ client/app/home/left-panel/left-panel.component.ts
@@ -3,20 +3,22 @@ import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { ApiAuthService } from 'client/app/shared/services/api/api-auth.service';
 import { EnvService } from 'client/app/shared/services/env.service';
-import { CookiesService } from 'client/app/shared/services/cookie.service';
 import en from 'client/app/shared/language/en';
+import { CookiesService } from 'client/app/shared/services/cookie.service';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  selector: 'app-left-panel',
+  templateUrl: './left-panel.component.html',
+  styleUrls: ['./left-panel.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class LeftPanelComponent implements OnInit {
 
   l = en
   env: any
 
-  constructor(private router: Router, private title: Title, private envService: EnvService, private apiAuthService: ApiAuthService, private cookiesService: CookiesService) { }
+  // TODO: @Output() event for minimizing the left panel
+
+  constructor(private router: Router, private title: Title, private envService: EnvService, private apiAuthService: ApiAuthService, public cookiesService: CookiesService) { }
 
   async ngOnInit() {
 
