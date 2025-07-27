0382909ba7dde9aa62f51bfb84056a8de5a75c29
GoldFrite <goldfrite@gmail.com>
2025-07-27 20:09:57 +0200

    Update db.ts

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,7 +1,9 @@
-import { PrismaClient } from '@prisma/client'
+import pkg from '@prisma/client'
+const { PrismaClient } = pkg
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+const globalForPrisma = globalThis as unknown as { prisma: typeof PrismaClient }
 
 export const db = globalForPrisma.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+
